ee5b3abfb92f7c61aa6b8fa4885ee71502079c4e
arley suarez agudelo <64574995+suarez020@users.noreply.github.com>
2024-12-17 10:05:16 -0500

    textColorHint

--- app/src/main/java/com/crystal/colmenacedi/ui/UbicarActivity.java
+++ app/src/main/java/com/crystal/colmenacedi/ui/UbicarActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
@@ -12,6 +13,7 @@ import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -139,6 +141,7 @@ public class UbicarActivity extends AppCompatActivity{
     }
 
     private void LLenarMenu() {
+        ocultarTeclado();
         Call<ResponseUbicacionGet> ubicacionGet = serviceRetrofit.doUbicacionGET(ean);
         ubicacionGet.enqueue(new Callback<ResponseUbicacionGet>() {
             @Override
@@ -170,6 +173,14 @@ public class UbicarActivity extends AppCompatActivity{
 
             }
         });
+    }
+
+    private void ocultarTeclado(){
+        View view = this.getCurrentFocus();
+        if(view != null){
+            InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 
     private void mostrarCategorias(){
